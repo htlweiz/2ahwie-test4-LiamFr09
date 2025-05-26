92ed80b8b99087a23a111b5bffaef039c18f8531
LiamFr09 <liam.fruehwirt09@gmail.com>
2025-05-26 08:55:58 +0000

    test_01

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -28,5 +28,32 @@ int main(int argc, char **argv)
   scanf("%d", &zahl);
   printf("Die eingegebene Zahl ist: %d (1 ... ungerade wurde übergeben, 2 ... gerade wurde übergeben, 0 ... die Zahl 0 wurde übergeben)\n", int_even_odd(zahl));
 
-  int array[10] = {}
+  int array[10] = {3, 1, 7, 4, 9, 1, 5, 10, -2, 2};
+  for(int i = 0; i < 10; i++)
+  {
+    printf("Zahl %d: %d\n", i+1, array[i]);
+  }
+
+  int int_numbers[5];
+  int gerade = 0;
+  int ungerade = 0;
+  int alle = 0;
+  for(int i = 0; i < 5; i++)
+  {
+    printf("%d. Zahl: ", i + 1);
+    scanf("%d", &int_numbers[i]);
+
+    if (i % 2 == 0)
+    {
+      gerade = gerade + 1;
+    }
+    else
+    {
+      ungerade = ungerade + 1;
+    }
+
+    alle = alle + 1;
+  }
 }
+
+
