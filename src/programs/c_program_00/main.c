bac196034baee24068c3906c39b0202bef0dd436
LiamFr09 <liam.fruehwirt09@gmail.com>
2025-05-26 09:03:02 +0000

    test_02

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -2,11 +2,11 @@
 
 int int_even_odd(int zahl)
 {
-  if(zahl == 0)
+  if (zahl == 0)
   {
     return 0;
   }
-  else if(zahl % 2 == 0)
+  else if (zahl % 2 == 0)
   {
     return 2;
   }
@@ -16,9 +16,7 @@ int int_even_odd(int zahl)
   }
 }
 
-
-
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
   printf("This program was called with %d arguments.\n", argc);
   printf("Hello World!\n");
@@ -29,31 +27,32 @@ int main(int argc, char **argv)
   printf("Die eingegebene Zahl ist: %d (1 ... ungerade wurde übergeben, 2 ... gerade wurde übergeben, 0 ... die Zahl 0 wurde übergeben)\n", int_even_odd(zahl));
 
   int array[10] = {3, 1, 7, 4, 9, 1, 5, 10, -2, 2};
-  for(int i = 0; i < 10; i++)
+  for (int i = 0; i < 10; i++)
   {
-    printf("Zahl %d: %d\n", i+1, array[i]);
+    printf("Zahl %d: %d\n", i + 1, array[i]);
   }
 
   int int_numbers[5];
-  int gerade = 0;
-  int ungerade = 0;
-  int alle = 0;
-  for(int i = 0; i < 5; i++)
+  int summe_gerade = 0;
+  int summe_ungerade = 0;
+  int summe_alle = 0;
+  for (int i = 0; i < 5; i++)
   {
     printf("%d. Zahl: ", i + 1);
     scanf("%d", &int_numbers[i]);
 
     if (i % 2 == 0)
     {
-      gerade = gerade + 1;
+      summe_gerade = summe_gerade + 1;
     }
     else
     {
-      ungerade = ungerade + 1;
+      summe_ungerade = summe_ungerade + 1;
     }
 
-    alle = alle + 1;
+    summe_alle = summe_alle + 1;
   }
+  printf("Anzahl der geraden Zahlen: %d\n", summe_gerade);
+  printf("Anzahl der ungeraden Zahlen: %d\n", summe_ungerade);
+  printf("Summe aller Zahlen: %d\n", summe_alle);
 }
-
-
