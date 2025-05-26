7ef7cc9e3b92ffb3da044041a43842114b6fbc5c
LiamFr09 <liam.fruehwirt09@gmail.com>
2025-05-26 08:45:13 +0000

    test

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int int_even_odd(int zahl)
+{
+  if(zahl == 0)
+  {
+    return 0;
+  }
+  else if(zahl % 2 == 0)
+  {
+    return 2;
+  }
+  else
+  {
+    return 1;
+  }
+}
+
+
+
+int main(int argc, char **argv) 
+{
   printf("This program was called with %d arguments.\n", argc);
   printf("Hello World!\n");
+
+  int zahl = 0;
+  printf("Geben Sie eine ganze Zahl ein: ");
+  scanf("%d", &zahl);
+  printf("Die eingegebene Zahl ist: %d (1 ... ungerade wurde übergeben, 2 ... gerade wurde übergeben, 0 ... die Zahl 0 wurde übergeben)\n", int_even_odd(zahl));
+
+  int array[10] = {}
 }
