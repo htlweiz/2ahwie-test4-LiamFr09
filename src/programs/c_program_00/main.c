752a9e9a8a8da087e3d25d153b3ef32cd1416a2c
LiamFr09 <liam.fruehwirt09@gmail.com>
2025-06-02 09:09:59 +0000

    test_verbesserung

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -18,6 +18,7 @@ int int_even_odd(int zahl)
 
 int main(int argc, char **argv)
 {
+  /*
   printf("This program was called with %d arguments.\n", argc);
   printf("Hello World!\n");
   printf("\n");
@@ -58,6 +59,48 @@ int main(int argc, char **argv)
   printf("Anzahl der geraden Zahlen: %d\n", summe_gerade);
   printf("Anzahl der ungeraden Zahlen: %d\n", summe_ungerade);
   printf("Summe aller Zahlen: %d\n", summe_alle);
+  */
+
+  // EXERCISE 1:
+  int number = 0, result = 0;
+  printf("Testzahl: ");
+  scanf("%d", &number);
+  result = int_even_odd(number);
+  if (result == 0) printf("Die Testzahl ist 0.\n");
+  else if (result == 1) printf("Die Testzahl ist ungerade.\n");
+  else printf("Die Testzahl ist gerade.\n");
+  
+  // EXERCISE 2:
+  int numbers[10]= {3, 1, 7, 4, 9, 1, 5, 10, -2, 2};
+  for (int i = 0; i < 10; i++)
+  {
+    printf("Zahl %d: %d\n", i+1, numbers[i]);
+
+  }
+
+  // EXERCISE 3:
+  int num_arr[5];
+  int count_even = 0, count_odd = 0, sum = 0;
+
+  for(int i = 0; i < 5; i++)
+  {
+    printf("%d. Zahl: ", i+1);
+    scanf("%d", num_arr[i]);
+  }
+  for(int i = 0; i < 5; i++)
+  {
+    sum = sum + num_arr[i];
+    if(num_arr[i] % 2 == 0) count_even++;                              // --> dann gerade
+  }
+  count_odd = 5 - count_even;
+
+  printf("Anzahl der geraden Zahlen: %d\n", count_even);
+  printf("Anzahl der ungeraden Zahlen: %d\n", count_odd);
+  printf("Summe der Zahlen: %d\n", sum);
+
+  
+
+
 }
 
 
