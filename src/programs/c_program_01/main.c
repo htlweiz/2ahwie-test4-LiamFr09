449544cfb444688820db74f9e0fc0c8b798841b8
LiamFr09 <liam.fruehwirt09@gmail.com>
2025-06-02 09:25:27 +0000

    array, uebung

--- src/programs/c_program_01/main.c
+++ src/programs/c_program_01/main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
-#include <test_c_lib.h>
 
-int main(int argc, char **argv) {
-  printf("Trying to confirm.\n");
-  say_hello();
+
+void init(int array[], int length, int value)
+{
+  for(int i = 0; i < 5; i++)
+  {
+    array[i] = value;
+  }
+}
+
+
+
+int main() 
+{
+  // function "init(int array[], int length, int value)"
+  // initialisiere ein Array "array" der Länge 5 mit dem Wert 7
+  int array[5];
+  init(array, 5, 7);
+  
+  
+  
+  
+  
+  
+  
+  
   return 0;
 }
